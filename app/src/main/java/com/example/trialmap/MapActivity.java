af76c3bfb336326c6f328930694f7667b392c834
Konrad Ewkiewicz <konrad.ewkiewicz@gmail.com>
2020-05-07 23:35:47 +0200

    Create project, add map, menu view

---
+++ app/src/main/java/com/example/trialmap/MapActivity.java
@@ -0,0 +1,39 @@
+package com.example.trialmap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+
+public class MapActivity extends AppCompatActivity {
+
+    MapView map = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+        Context ctx = getApplicationContext();
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+
+        setContentView(R.layout.activity_main);
+
+        map = (MapView) findViewById(R.id.map);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+        map.setMultiTouchControls(true);
+
+        IMapController mapController = map.getController();
+        mapController.setZoom(12.0);
+        GeoPoint startPoint = new GeoPoint(100, 100);
+        mapController.setCenter(startPoint);
+
+
+    }
+}
